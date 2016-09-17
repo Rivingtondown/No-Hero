50c081782ed4d4897f4e4e825f16aee20ab5c788
Rivingtondown <huey3000>
2016-09-16 21:30:57 -0400

    "Farming" String changed in common events and scripts

--- js/plugins/HarvestHelper.js
+++ js/plugins/HarvestHelper.js
@@ -13,7 +13,7 @@ by: RivingtonDown
 Item Note: harvest type, harvest level, item id
 
 E.g.
-Crop 1 5 (Farming crop at level 1 that will provide you item id:5)
+Crop 1 5 (Homesteader crop at level 1 that will provide you item id:5)
 Forage 3 1 (Foraging plant at level 3 that will provice you item id:1)
 
 */
@@ -30,11 +30,11 @@ var HarvestItem = null;
 var HarvestEvent = null;
 
 var HarvestYield = null;
-var CraftXP = null;
-var CraftLvl = null;
-var CraftXPVar = null;
-var CraftLvlVar = null;
-var CraftType = null;
+var ProfXP = null;
+var ProfLvl = null;
+var ProfXPVar = null;
+var ProfLvlVar = null;
+var ProfType = null;
 var mapID = null;
 var eventID = null;
 
@@ -50,31 +50,31 @@ Game_System.prototype.createHarvestable = function (value, eventID, mapID) {
 
   if (HarvestType == "Forage") {
     //if you're foraging
-    CraftType = "Foraging"; //string for messages
-    CraftXPVar = 6; //in-game forage xp variable number
-    CraftLvlVar = 7; //in-game forage lvl variable number
+    ProfType = "Foraging"; //string for messages
+    ProfXPVar = 6; //in-game forage xp variable number
+    ProfLvlVar = 7; //in-game forage lvl variable number
     ;
   }
   if (HarvestType == "Crop") {
     //if you're farming
-    CraftType = "Farming"; //string for messages
-    CraftXPVar = 9; //in-game farming xp variable number
-    CraftLvlVar = 8; //in-game farming lvl variable number
+    ProfType = "Homesteader"; //string for messages
+    ProfXPVar = ProfBuild.parameters.HomesteaderXP; //in-game Homesteader xp variable number
+    ProfLvlVar = ProfBuild.parameters.HomesteaderLVL; //in-game Homesteader lvl variable number
   }
-  CraftXP = $gameVariables.value(CraftXPVar); //in-game craft xp quick value
-  CraftLvl = $gameVariables.value(CraftLvlVar); //in-game craft lvl quick value
+  ProfXP = $gameVariables.value(ProfXPVar); //in-game profession xp quick value
+  ProfLvl = $gameVariables.value(ProfLvlVar); //in-game profession lvl quick value
 
-  console.log("Trying to harvest... " + CraftType + " Lvl:" + CraftLvl + " / Harvest Lvl:" + HarvestLvl);
+  console.log("Trying to harvest... " + ProfType + " Lvl:" + ProfLvl + " / Harvest Lvl:" + HarvestLvl);
 
-  if (HarvestType == "Forage" && HarvestLvl > CraftLvl) {
+  if (HarvestType == "Forage" && HarvestLvl > ProfLvl) {
     //if you're foraging AND the plant is higher level than your foraging lvl
     $gameMessage.add( //create a message
-    CraftType + " level too low to forage "+$gameMap.event(eventID).event().name.toLowerCase()+"\n" + "Required level: " + HarvestLvl + "\n" + "Current level: " + CraftLvl + "\n");
+    ProfType + " level too low to forage "+$gameMap.event(eventID).event().name.toLowerCase()+"\n" + "Required level: " + HarvestLvl + "\n" + "Current level: " + ProfLvl + "\n");
     return; //cancel out of the script
   } else {
     //otherwise, assuming your are either farming or you're foraging but the plant is not higher level
-    calculateItems(CraftLvl, HarvestItem); //generate the items and give them to the player
-    calculateExp(HarvestLvl, CraftLvl); //calculate experience and level ups for the player's craft skill
+    calculateItems(ProfLvl, HarvestItem); //generate the items and give them to the player
+    calculateExp(HarvestLvl, ProfLvl); //calculate experience and level ups for the player's profession
     if (HarvestType == "Forage") {
       //if you're foraging
       $gameSelfSwitches.setValue([mapID, eventID, "A"], true); //switch plant to self switch A, which should delete the event
@@ -86,10 +86,10 @@ Game_System.prototype.createHarvestable = function (value, eventID, mapID) {
     if (HarvestType == "Forage") {
       //if you are foraging
       HarvestYield = Math.floor(Math.random() * Math.min(4, level)) + 1; //calculate yield based on item level
-      if (CraftLvl > HarvestLvl) {
+      if (ProfLvl > HarvestLvl) {
         //if you have a higher forage lvl than the harvest lvl
         var random = Math.random();
-        bonusSuccess = (CraftLvl - HarvestLvl) * 10; //generate percentage chance based on level difference
+        bonusSuccess = (ProfLvl - HarvestLvl) * 10; //generate percentage chance based on level difference
         if (random < bonusSuccess) {
           //if you suceed at that chance
           AudioManager.playSe({ name: "Item3", volume: 100, pitch: 100, pan: 0, pos: 0 }); //play bonus gain sfx
@@ -100,11 +100,11 @@ Game_System.prototype.createHarvestable = function (value, eventID, mapID) {
     }
     if (HarvestType == "Crop") {
       //if you are harvesting a crop
-      if (CraftLvl - HarvestLvl == 0) {
-        //if craft level is only one and you try to harvest a level one crop
+      if (ProfLvl - HarvestLvl == 0) {
+        //if profession level is only one and you try to harvest a level one crop
         HarvestYield = 1; //set the yield to one so it isn't zero
       } else {
-        HarvestYield = Math.min(4, CraftLvl - HarvestLvl + 1); //otherwise yield is the level difference to a maximum of 4
+        HarvestYield = Math.min(4, ProfLvl - HarvestLvl + 1); //otherwise yield is the level difference to a maximum of 4
       }
     }
     $gameParty.gainItem($dataItems[HarvestItem], HarvestYield); //gain the items you were foraging or harvesting in the correct yield
@@ -112,20 +112,20 @@ Game_System.prototype.createHarvestable = function (value, eventID, mapID) {
   }
 
   function calculateExp(experience, level) {
-    var toNextLvl = Math.pow(level + 1, 2) * 2; //Formula that generates xp to next craft level
+    var toNextLvl = Math.pow(level + 1, 2) * 2; //Formula that generates xp to next profession level
 
-    CraftXP = $gameVariables.value(CraftXPVar) + experience; //add harvest level to craft xp
-    $gameVariables.setValue(CraftXPVar, CraftXP); //set that xp to in-game craft xp variable
+    ProfXP = $gameVariables.value(ProfXPVar) + experience; //add harvest level to profession xp
+    $gameVariables.setValue(ProfXPVar, ProfXP); //set that xp to in-game profession xp variable
 
-    if (CraftXP >= toNextLvl) {
-      //if you've gained enough craft xp level up craft lvl
-      CraftXP = toNextLvl - CraftXP; //reset craft xp, allowing carry over of extra xp into next level
-      $gameVariables.setValue(CraftXPVar, CraftXP); //set that craft xp variable in-game
-      CraftLvl = level + 1; //add 1 to crafting lvl
-      $gameVariables.setValue(CraftLvlVar, CraftLvl); //set that level to in-game variable
+    if (ProfXP >= toNextLvl) {
+      //if you've gained enough profession xp level up profession lvl
+      ProfXP = toNextLvl - ProfXP; //reset profession xp, allowing carry over of extra xp into next level
+      $gameVariables.setValue(ProfXPVar, ProfXP); //set that profession xp variable in-game
+      ProfLvl = level + 1; //add 1 to profession lvl
+      $gameVariables.setValue(ProfLvlVar, ProfLvl); //set that level to in-game variable
       $gameMessage.setPositionType(2); //set message to screen middle
-      AudioManager.playSe({ name: "Saint5", volume: 100, pitch: 100, pan: 0, pos: 0 }); //play craft level up sfx
-      $gameMessage.add(CraftType + " Level Increased to " + $gameVariables.value(CraftLvlVar) + "!"); //generate craft level up message
+      AudioManager.playSe({ name: "Saint5", volume: 100, pitch: 100, pan: 0, pos: 0 }); //play profession level up sfx
+      $gameMessage.add(ProfType + " Level Increased to " + $gameVariables.value(ProfLvlVar) + "!"); //generate profession level up message
     }
   }
 };
@@ -134,28 +134,28 @@ Game_System.prototype.plowPlot = function (value, eventID, mapID) {
   HarvestLvl = parseInt(value[0]); //level of the crop you're harvesting
   HarvestItem = parseInt(value[1]); //item code of the crop you're harvesting
 
-  CraftLvl = $gameVariables.value(8);
-
-  if (HarvestLvl > CraftLvl) {
+  ProfLvl = $gameVariables.value(ProfBuild.parameters.HomesteaderLVL);
+  if (HarvestLvl > ProfLvl) {
     //if the crop is too high level for you to harvest
     $gameMessage.add( //create a message
-    "Farming level too low to grow "+$gameMap.event(eventID).event().name.toLowerCase()+"\n" + "Required level: " + HarvestLvl + "\n" + "Current level: " + CraftLvl + "\n");
-    $gameMessage.add("Clear plot?"); //generate an option to clear the plot
-    $gameMessage.setChoices(['Yes', 'No'], 0, -1);
+    "Homesteader level too low to harvest "+$gameMap.event(eventID).event().name.toLowerCase()+"\n" + "Required level: " + HarvestLvl + "\n" + "Current level: " + ProfLvl + "\n");
+    $gameMessage.add("Clear the plot?"); //generate an option to plow anyways or clear the plot
+    $gameMessage.setChoices(['Clear the plot', 'Cancel'], 0, -1);
     $gameMessage.setChoiceCallback(function (choice) {
       if (choice == 0) {
-        //if you choose yes
+        //if you choose Clear the plot
         $gameSelfSwitches.setValue([mapID, eventID, "D"], true); //set self switch to D which should automatically destroy event
+        return;
       } else {
-          //if you choose no
-          return; //cancel out of the script
-        }
+        //if you choose Cancel
+        return;
+      }
     });
   } else {
-    //if you are, in-fact, high enough level
     $gameSelfSwitches.setValue([mapID, eventID, "A"], true); //turn self-switch to A which should allow you to water
     $gameVariables.setValue(10, $gameVariables.value(10) - 1); //lower your gardening tools durability by 1
   }
+
 };
 Game_System.prototype.plantPlots = function (value, eventID, mapID) {
   $gameMessage.add("What will you plant?");
